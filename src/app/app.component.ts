412ade957f52b6e4c06391d2d9538ca758af7720
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-03-23 20:43:00 -0500

    this page is now SPA (Single Page Application) with it's new functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductoListaComponent } from "./producto-lista/producto-lista.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProductoListaComponent],
+  imports: [RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
