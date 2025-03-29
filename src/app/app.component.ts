a50e33a9e64eeac7607fdfac67fef6dc66a10f84
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-03-28 22:29:15 -0500

    now i added a new field  in editar-producto an agregar-producto for its proper functioning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductoListaComponent } from "./producto-lista/producto-lista.component";
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  idEncontrar!: number;
+  buscarProducto(){
+
+  }
 }
